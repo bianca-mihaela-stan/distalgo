80eb2425b2816073267fd966cb366be51db664f4
Bianca <stanbianca611@gmail.com>
2023-01-03 15:19:43 +0200

    Benchmarks from sh working great.

--- da/examples/MIS/test.sh
+++ da/examples/MIS/test.sh
@@ -4,7 +4,7 @@ for (( i=0; i<10; i++ ))
 do
     for (( j = 0; j <10; j++))
     do
-        python -m da lubys.da 1000 i j
-        python -m da ghaffari.da 1000 i j
+        python -m da lubys.da 1000 "$i" "$j"
+        python -m da ghaffari.da 1000 "$i" "$j"
     done
 done
